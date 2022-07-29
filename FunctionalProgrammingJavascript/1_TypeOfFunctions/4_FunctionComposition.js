d029f7abecefcbf138ca08ca54b443d4e18ed667
Jorge Reyes <rjorge99@hotmail.com>
2022-07-29 12:26:21 -0500

    Exercise added for composition

--- FunctionalProgrammingJavascript/1_TypeOfFunctions/4_FunctionComposition.js
+++ FunctionalProgrammingJavascript/1_TypeOfFunctions/4_FunctionComposition.js
@@ -20,3 +20,21 @@ console.log(wordCount3(sentence)); // 20
 // Same as compose, but from left to right
 const wordCount4 = R.pipe(R.split(' '), R.length);
 console.log(wordCount4(sentence)); // 20
+
+// Exercise
+// Count how many digits there are in the following
+// sentence, using functional composition
+const sentence2 =
+    'PechaKucha is a presentation style in which 20 slides are shown for 20 seconds each (6 minutes and 40 seconds in total).';
+
+
+const numbersInString = R.pipe(
+    R.split(''),
+    R.map(parseInt),
+    R.filter(Number.isInteger),
+    R.length
+);
+
+expect(numbersInString(sentence)).toBe(7);
+
+console.log('If you see this printed in the console, the test passed!');
