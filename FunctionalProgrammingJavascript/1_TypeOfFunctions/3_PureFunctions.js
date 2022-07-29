3dafc06cd4690c42acec7f5a0446c08827c076fd
Jorge Reyes <rjorge99@hotmail.com>
2022-07-29 09:38:01 -0500

    Pure functions

---
+++ FunctionalProgrammingJavascript/1_TypeOfFunctions/3_PureFunctions.js
@@ -0,0 +1,18 @@
+// Pure Function: Creates and returns a new value based on the input parameters
+// No side effects
+// Must have input parameters
+// Doesnt depend on outside variables
+//      Reusable
+//      Composable
+//      Easy to test
+
+// Pure Function
+function add(a, b) {
+    return a + b;
+}
+
+// Impure Function (Procedure)
+let counter = 0;
+function increment() {
+    counter++;
+}
