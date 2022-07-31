318909f3b2bb1d0d89600a7ccf9773285a93a1b7
Jorge Reyes <rjorge99@hotmail.com>
2022-07-31 18:48:45 -0500

    Calories app addde

--- FunctionalProgrammingJavascript/4_Repo/caloriecounter/src/index.js
+++ FunctionalProgrammingJavascript/4_Repo/caloriecounter/src/index.js
@@ -1,1 +1,7 @@
-console.log('It Worked!');
+import initModel from './model';
+import update from './update';
+import view from './view';
+import app from './app';
+
+const node = document.getElementById('app');
+app(initModel, update, view, node);
