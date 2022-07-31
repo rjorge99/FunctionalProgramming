44952f73d110c60a9aeb36bdbf6a14e699e81f8f
Jorge Reyes <rjorge99@hotmail.com>
2022-07-31 11:05:04 -0500

    Module3 added

--- FunctionalProgrammingJavascript/2_HtmlAndCss/1_index.js
+++ FunctionalProgrammingJavascript/2_HtmlAndCss/1_index.js
@@ -7,6 +7,49 @@ const meals = [
     { name: 'Coffee', price: '2.00' }
 ];
 
+const { td, th, tr, tbody, thead, table } = tags;
+
+// cell <--
+// mealRow <--
+// headerRow <--
+// totalRow <--
+// mealBody
+// mealHeader <--
+// mealsTable
+
+// Using Hyperscript
 function cell(tag, className, value) {
     return tag({ className }, value);
 }
+
+function mealRow(className, meal) {
+    return tr({ className }, [cell(td, 'pa2', meal.name), cell(td, 'pa2 tr', meal.price)]);
+}
+
+function mealsBody(className, meals) {
+    const rows = R.map(R.partial(mealRow, ['stripe-dark']), meals);
+    const rowsWithTotal = R.append(totalRow(meals), rows);
+    return tbody({ className }, rowsWithTotal);
+}
+
+const headerRow = tr([cell(th, 'pa2 tl', 'Meal'), cell(th, 'pa2 tr', 'Price')]);
+const mealHeader = thead(headerRow);
+
+function totalRow(meals) {
+    const total = R.pipe(
+        R.map((meal) => meal.price),
+        R.reduce(R.add, 0)
+    )(meals);
+    return tr({ className: 'bt b' }, [cell(td, 'pa2 tr', 'Total:'), cell(td, 'pa2 tr', total)]);
+}
+
+function mealsTable(meals) {
+    return table({ className: 'mw5 center w-100 collapse' }, [mealHeader, mealsBody('', meals)]);
+}
+
+const node = document.getElementById('app');
+const view = mealsTable(meals);
+node.appendChild(view);
+
+// Other alternative
+// https://jsbin.com/nusobel/edit?js,output
---
+++ FunctionalProgrammingJavascript/3_FunctionalConcepts/1_ImperativeDeclarative.js
@@ -0,0 +1,7 @@
+// Imperative
+$('#app').append(`<table class='mw5 center'></table>`);
+$('#app table').append(`<thead></thead>`);
+
+// ...
+
+// Declarative is the sample showed in the module 2, using ramda.js
