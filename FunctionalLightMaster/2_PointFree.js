99c80601e5f43f8f0683e699d69bb5bb7aa4137e
Jorge Reyes <rjorge99@hotmail.com>
2022-08-04 21:05:21 -0500

    advanced free points samples

--- FunctionalLightMaster/2_PointFree.js
+++ FunctionalLightMaster/2_PointFree.js
@@ -22,29 +22,26 @@ function isEven(v) {
 
 // const isEven = not(isOdd); // <--
 
-// Excercise:
+// --------------------------------Excercise --------------------------------
+const msg1 = 'Hello';
+const msg2 = msg1 + ' World';
 // function output(msg) {
 //     console.log(msg);
 // }
-
 // function printIf(shouldPrintIf) {
 //     return function (msg) {
 //         if (shouldPrintIf) output(msg);
 //     };
+// }
+// function isLongEnough(str) {
+//     return !isShortEnough(str);
 // }
 
 function isShortEnough(str = '') {
     return str.length <= 5;
 }
 
-// function isLongEnough(str) {
-//     return !isShortEnough(str);
-// }
-
-const msg1 = 'Hello';
-const msg2 = msg1 + ' World';
-
-// Solution
+// ---------------------------------Solution ---------------------------------
 const output = console.log; // depending of the javascript environment, log function requires to be called from console
 const isLongEnough = not(isShortEnough);
 const printIf = when(output);
@@ -53,3 +50,23 @@ printIf(isShortEnough)(msg1); // Hello
 printIf(isShortEnough)(msg2);
 printIf(isLongEnough)(msg1);
 printIf(isLongEnough)(msg2); // Hello World
+
+// ---------------------------------Advanced Free Points ---------------------------------
+const mod = (y) => (x) => x % y;
+const eq = (y) => (x) => x === y;
+
+const mod2 = mod(2);
+const eq1 = eq(1);
+
+function isOdd(x) {
+    return eq1(mod2(x));
+}
+
+function compose(fn2, fn1) {
+    return function (v) {
+        return fn2(fn1(v));
+    };
+}
+
+const isOdd = compose(eq1, mod2); // Point free definition for isOdd
+const isOdd = compose(eq(1), mod(2)); // Final point free definition for isOdd
