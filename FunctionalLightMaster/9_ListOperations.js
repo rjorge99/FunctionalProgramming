85b08a1793efa2adc9fa41278edd8a8b6a01330c
Jorge Reyes <rjorge99@hotmail.com>
2022-08-06 22:38:34 -0500

    List operations added and finished

--- FunctionalLightMaster/9_ListOperations.js
+++ FunctionalLightMaster/9_ListOperations.js
@@ -58,7 +58,7 @@ const nine = constant(9);
 
 // Reduce
 // const addn = (...fns) => fns.reduce((v, fn) => v + fn(v), 0);
-function addn(...fns) {
+function addn(fns) {
     return fns.reduce(function (composedFns, fn) {
         return function () {
             return add2(composedFns, fn);
@@ -66,5 +66,25 @@ function addn(...fns) {
     })();
 }
 
-const result = addn(constant(3), constant(4), constant(6)); // 7
-console.log(result);
+// const result = addn(constant(3), constant(4), constant(6)); // 13
+
+var numbers = [1, 5, 6, 7, 8, 9, 5, 4, 10, 11, 18, 19, , 5, 12, 11];
+var noRepeats = numbers
+    .reduce((arr, c) => {
+        if (!arr.includes(c)) arr = [...arr, c];
+        return arr;
+    }, [])
+    .filter((x) => x % 2 === 0)
+    .map(constant);
+
+addn(noRepeats); // 58
+
+// Fusion
+const add1 = (x) => x + 1;
+const mul2 = (x) => x * 2;
+const div3 = (x) => x / 3;
+const list = [2, 5, 8, 11, 14, 17, 20];
+
+list.map(add1).map(mul2).map(div3);
+
+list.map(compose(div3, mul2, add1)); // <-- fusion
