62edb7afbeeaab437d76109d237f153f67ea7e88
Jorge Reyes <rjorge99@hotmail.com>
2022-12-03 20:38:53 -0600

    Se cambia nombre de archivo

--- Functional-Lite-Javascript-pluralsight/1_Pure_Functions.js
+++ Functional-Lite-Javascript-pluralsight/1_Pure_Functions.js
@@ -1,3 +1,4 @@
 // Side effects -> something that ocurres that changes the outside state of the program.
 // (console.log, change value of vareables, etc) should be avoided
+
 // Pure functions -> no side effects, given a set of parameters, always returns the same result
